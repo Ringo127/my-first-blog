6d97da82985936a841c662c6df281d3fe42f2c80
ringo <noyotaku127@gmail.com>
2022-03-15 22:44:17 +0900

    cash clear

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
